6a5218d7d97a935997f232fb5b816ae1805d403f
ParkPing <105095888+securityprotect@users.noreply.github.com>
2026-02-08 15:09:54 +0530

    Update import paths to include .js extension

--- api/stats.js
+++ api/stats.js
@@ -1,6 +1,7 @@
-import { connectDB } from "../lib/db";
-import Card from "../lib/Card";
-import { verifyAuth } from "../lib/auth";
+import { connectDB } from "../lib/db.js";
+import Card from "../lib/Card.js";
+import { verifyAuth } from "../lib/auth.js";
+
 
 export default async function handler(req, res) {
   try {
@@ -18,3 +19,4 @@ export default async function handler(req, res) {
     res.status(401).json({ message: err.message });
   }
 }
+
